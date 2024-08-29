8dbb930f6cf5b0dd6aeb808cff8564bd484586ce
donggeon Na <ndmb2012@gmail.com>
2024-08-29 22:21:49 +0900

    feat: plot view

---
+++ chart_view.py
@@ -0,0 +1,53 @@
+import plotille
+import pandas as pd
+import candle_store
+import pandas as pd
+import candle_store
+
+def drawMarket(fig, data):
+    # fig.plot(
+    #     data['reg'],
+    #     data['open'],
+    #     lc=4,
+    #     label='First line',
+    # )
+    # fig.plot(
+    #     data['reg'],
+    #     data['close'],
+    #     lc=4,
+    #     label='First line'
+    # )
+    fig.scatter(
+        data['reg'],
+        data['high'],
+        lc=5,
+    )
+    fig.scatter(
+        data['reg'],
+        data['low'],
+        lc=6
+    )
+
+def drawMA(fig, window_size, lc):
+    data = candle_store.getMA(window_size)
+    fig.plot(
+        data['reg'],
+        data['SMA'],
+        lc=lc
+    )
+
+def showCandlePlot():
+    plotData = candle_store.plotData()
+
+    fig = plotille.Figure()
+    fig.width = 90
+    fig.height = 50
+    fig.color_mode = 'byte'
+    
+    drawMarket(fig, plotData)
+    drawMA(fig, window_size=15, lc=1)
+    drawMA(fig, window_size=50, lc=2)
+
+    print(fig.show())
+
+showCandlePlot()
