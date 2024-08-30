7bd522ce889d45d9f27a1e419a46e91282e001f6
donggeon Na <ndmb2012@gmail.com>
2024-08-30 18:43:25 +0900

    style: seperate with stars

--- stat_view.py
+++ stat_view.py
@@ -1,10 +1,19 @@
 import upbit
+import stat_store as StatStore
 
 def balance_view():
-    print(f'cash balance: {upbit.getCashBalance()} WON')
-    print(f'coin balance: {upbit.getCoinBalance()} BTC')
+    print(f'****    cash balance: {upbit.getCashBalance()} WON')
+    print(f'****    coin balance: {upbit.getCoinBalance()} BTC')
 
 def returnView():
-    print(f'getBuyPrice: {upbit.getBuyPrice()}')
-    # print(f'return: {} WON')
-    # print(f'rate of return: {} %')
+    print(f'****    getRateOfReturn: {StatStore.getRateOfReturn()} %')
+    print(f'****    profit: {StatStore.getProfit()} WON')
+
+def show():
+    print("*****************************************************************")
+    print("****")
+    balance_view()
+    returnView()
+    print("****")
+    print("*****************************************************************")
+
